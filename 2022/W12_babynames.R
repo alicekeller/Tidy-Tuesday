f90b2a6fac55014c22437e3c88e3e56490326da6
Ali Keller <47513611+alicekeller@users.noreply.github.com>
2022-03-28 11:37:59 -0600

    Update W12_babynames.R

--- 2022/W12_babynames.R
+++ 2022/W12_babynames.R
@@ -22,14 +22,27 @@ the_friends <- babynames %>%
   group_by(year, name) %>% 
   summarise(total_prop = sum(prop))
 
+
+#create data frame for labeling for v-line
+line_label <- data.frame(name = c("Chandler"), label = c("Friends premieres \non TV"))
+#create data frame for arrow
+data_segm <- data.frame(x = 1980, y = 0.0065, xend = 1993, yend = 0.0065, name = "Chandler")
+  
+
 #create plot
 the_friends %>% 
   ggplot(aes(year, total_prop)) +
   geom_area(aes(fill = the_friends$name)) +
   facet_wrap(~name, ncol = 2) +
+  geom_vline(xintercept = 1994, size = 0.7, color = "#9C8CD4") +
   scale_y_continuous(labels = scales::percent, breaks = c(0.001, 0.002, 0.004)) +
   scale_x_continuous((limits = c(1885, 2020))) +
   theme_minimal() +
+  geom_text(x = 1972, y = 0.0075, aes(label = label), color = "#9C8CD4", family = "galdeano", size = 6, data = line_label) +
+  geom_segment(data = data_segm, aes(x = x, y = y, yend = yend, xend = xend), 
+               arrow = arrow(length = unit(0.4, "cm")),
+               inherit.aes = FALSE, 
+               color = "#9C8CD4") +
   scale_fill_manual(values=c("#FF4238", "#FFDC00", "#42A2D6", "#9A0006", "#fff580", "#00009E")) +
   labs(title = "THE ONE WITH ALL THE BABY NAMES",
        subtitle = "Popularity of the Friends names over time \nby percentage of total babies named",
@@ -48,4 +61,3 @@ the_friends %>%
         panel.background = element_rect(fill = "black", color = "black"),
         strip.placement = "inside",
         plot.caption = element_text(size = 14, color = "grey"))
-
